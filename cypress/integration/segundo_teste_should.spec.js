94274deab0d093e54dc57db1b7dbd1fc13a0e34c
Erika Santana <erika.araujo12344@gmail.com>
2021-07-31 11:14:42 -0300

    em andamento

--- cypress/integration/segundo_teste_should.spec.js
+++ cypress/integration/segundo_teste_should.spec.js
@@ -5,10 +5,39 @@ context('Validar menus', () => {
     it('clicando no link comprar deve direcionar para a página de compra', () => {
         cy.visit('http://lojaebac.ebaconline.art.br/')
         cy.get('#primary-menu > .menu-item-629 > a').as('ComprarMenuLink')
-        cy.get('@ComprarMenuLink').contains('Comprar').and('have.attr', 'href')
-        cy.get('@ComprarMenuLink').click()
+        cy.get('@ComprarMenuLink').contains('Comprar').and('have.attr', 'href').and('include', 'shop')
+        // cy.get('@ComprarMenuLink').click()
 
-        cy.contains('Produtos')
+        // cy.contains('Produtos')
         //cy.url().contains('/shop')
+
+        cy.get('.page-title').should('contain', 'Produtos')
     });
 });
+
+
+/*
+cy.visit('http://lojaebac.ebaconline.art.br/')
+
+cy.get('#primary-menu > .menu-item-629 > a').contains('Comprar')
+cy.get('#primary-menu > .menu-item-629 > a').click()
+
+//Refatorar: Usa-se os as('') com um apelido
+
+cy.get('#primary-menu > .menu-item-629 > a').as('ComprarMenuLink')
+cy.get('@ComprarMenuLink').contains('Comprar')
+cy.get('@ComprarMenuLink').click()
+
+//Combinando Ações
+cy.get('@ComprarMenuLink').contains('Comprar').click()
+
+//Verificando Atributos
+cy.get('@ComprarMenuLink').contains('Comprar').and('have.attr', 'href')
+cy.get('@ComprarMenuLink').click()
+
+
+//---
+cy.contains('Produtos')
+
+
+*/
